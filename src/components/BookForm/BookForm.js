ff3c0dc90320c23e94f69c4f45666d013e51eb34
anarmh <anarmh@code.edu.az>
2024-10-22 20:33:19 +0400

    initialize Redux,add and display books

---
+++ src/components/BookForm/BookForm.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBook } from "../../redux/books/actionCreators";
+import "./BookForm.css";
+
+const BookForm = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && author) {
+      const book = {
+        title,
+        author,
+      };
+      console.log(addBook(book))
+      dispatch(addBook(book));
+      setTitle("");
+      setAuthor("");
+    }
+  };
+  return (
+    <div className="app-block book-form">
+      <h2>Add a New Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Add Book</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default BookForm;
