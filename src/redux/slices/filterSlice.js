84d8f6b07172119455e7c5380d73ea648851c4c4
anarmh <anarmh@code.edu.az>
2024-12-09 16:31:34 +0400

    Filter Redux slice  by title

---
+++ src/redux/slices/filterSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  title: "",
+  author:"",
+};
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    setTitleFilter: (state, action) => {
+      return { ...state, title: action.payload };
+    },
+    setAuthorFilter:(state,action)=>{
+      state.author=action.payload
+    },
+    resetFilters:()=>{
+      return initialState
+    }
+  },
+});
+
+export const { setTitleFilter,setAuthorFilter,resetFilters } = filterSlice.actions;
+export const selectTitleFilter = (state) => state.filter.title;
+export const selectAuthorFilter=(state)=>state.filter.author;
+export default filterSlice.reducer;
